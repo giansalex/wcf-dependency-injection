dbac84f61aea5968de691234d2c05cb926076d80
Giancarlos Salas <giansalex@gmail.com>
2018-03-06 13:35:34 -0500

    configure DI with autofac

---
+++ WcfService/Config/Bootstrap.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using WcfService.Services;
+
+namespace WcfService.Config
+{
+    public static class Bootstrap
+    {
+        public static IContainer Execute()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<CalculatorService>();
+            builder
+                .RegisterType<SumOperation>()
+                .As<IOperation>()
+                .SingleInstance();
+            
+
+            return builder.Build();
+        }
+    }
+}
---
+++ WcfService/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using Autofac.Integration.Wcf;
+using System;
+using WcfService.Config;
+
+namespace WcfService
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AutofacHostFactory.Container = Bootstrap.Execute();
+        }
+    }
+}
