2acfc14cdd53d2f0c1428ad7e1594dafe64587c8
Giancarlos Salas <giansalex@gmail.com>
2018-03-06 13:44:44 -0500

    refact tree

--- WcfService/Services/CalculatorService.svc.cs
+++ WcfService/Services/CalculatorService.svc.cs
@@ -1,6 +1,4 @@
-﻿using WcfService.Services;
-
-namespace WcfService
+﻿namespace WcfService.Services
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
