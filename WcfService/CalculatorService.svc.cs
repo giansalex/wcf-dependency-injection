459f953f08cc40df33e04c906e19080908565d31
Giancarlos Salas <giansalex@gmail.com>
2018-03-06 13:28:28 -0500

    update wcf services

---
+++ WcfService/CalculatorService.svc.cs
@@ -0,0 +1,21 @@
+﻿using WcfService.Services;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class CalculatorService : ICalculatorService
+    {
+        private readonly IOperation _operation;
+
+        public CalculatorService(IOperation operation)
+        {
+            _operation = operation;
+        }
+
+        public int Operation(int a, int b)
+        {
+            return _operation.Execute(a, b);
+        }
+    }
+}
