a43594cf9c040fd6983e697c56fec168ef242b11
Giancarlos Salas <giansalex@gmail.com>
2018-03-06 14:36:14 -0500

    complete unit test

---
+++ WcfService.Tests/Services/CalculatorServiceTests.Moq.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService.Services;
+
+namespace WcfService.Tests.Services
+{
+    public partial class CalculatorServiceTests
+    {
+        private ICalculatorService CreateAdd()
+        {
+            return GetByOperation((a, b) => a + b);
+        }
+
+        private ICalculatorService CreateSubstract()
+        {
+            return GetByOperation((a, b) => a - b);
+        }
+
+        private ICalculatorService CreateMultiply()
+        {
+            return GetByOperation((a, b) => a * b);
+        }
+
+        private ICalculatorService CreatePow()
+        {
+            return GetByOperation((a, b) => (int)Math.Pow(a, b));
+        }
+
+
+        private ICalculatorService GetByOperation(Func<int, int, int> operation)
+        {
+            var mock = new Mock<IOperation>();
+            mock
+                .Setup(r => r.Execute(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(operation);
+
+            return new CalculatorService(mock.Object);
+        }
+    }
+}
--- WcfService.Tests/Services/CalculatorServiceTests.cs
+++ WcfService.Tests/Services/CalculatorServiceTests.cs
@@ -1,29 +1,49 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
 namespace WcfService.Tests.Services
 {
-    [TestClass]
-    public class CalculatorServiceTests
+    [TestFixture]
+    public partial class CalculatorServiceTests
     {
-        [TestMethod]
+        static int[][] Numbers =
+        {
+            new [] { 2, 5 },
+            new [] { 4, 2 },
+            new [] { 8, 9 }
+        };
+
+        [Test]
         public void OperationAdd()
         {
+            var service = CreateAdd();
+
+            Assert.AreEqual(5, service.Operation(2, 3));
+            Assert.AreNotEqual(4, service.Operation(1, 2));
         }
 
-        [TestMethod]
+        [Test]
         public void OperationSubstract()
         {
+            var service = CreateSubstract();
+
+            Assert.AreEqual(3, service.Operation(5, 2));
         }
 
-        [TestMethod]
-        public void OperationMultiply()
+        [Test, TestCaseSource("Numbers")]
+        public void OperationMultiply(int a, int b)
         {
+            var service = CreateMultiply();
+
+            Assert.AreEqual(a * b, service.Operation(a, b));
         }
 
 
-        [TestMethod]
+        [Test]
         public void OperationPow()
         {
+            var service = CreatePow();
+
+            Assert.AreEqual(1, service.Operation(1, 2));
         }
     }
 }
