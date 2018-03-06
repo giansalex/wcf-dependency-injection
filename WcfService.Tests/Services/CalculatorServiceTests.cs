72be7dabfc63b7a4a8696160b5c700f265ba85a4
Giancarlos Salas <giansalex@gmail.com>
2018-03-06 14:05:01 -0500

    add project test

---
+++ WcfService.Tests/Services/CalculatorServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcfService.Tests.Services
+{
+    [TestClass]
+    public class CalculatorServiceTests
+    {
+        [TestMethod]
+        public void OperationAdd()
+        {
+        }
+
+        [TestMethod]
+        public void OperationSubstract()
+        {
+        }
+
+        [TestMethod]
+        public void OperationMultiply()
+        {
+        }
+
+
+        [TestMethod]
+        public void OperationPow()
+        {
+        }
+    }
+}
