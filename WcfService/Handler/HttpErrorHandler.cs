ab90e27cdf870d18102c6e9b5e33048661b1e937
Giancarlos Salas <giansalex@gmail.com>
2018-03-07 09:33:01 -0500

    add handler

---
+++ WcfService/Handler/HttpErrorHandler.cs
@@ -0,0 +1,22 @@
+﻿using Elmah;
+using System;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+
+namespace WcfService.Handler
+{
+    public class HttpErrorHandler : IErrorHandler
+    {
+        public bool HandleError(Exception error)
+        {
+            return false;
+        }
+
+        public void ProvideFault(Exception error, MessageVersion version, ref Message fault)
+        {
+            if (error == null) return;
+
+            ErrorLog.GetDefault(null).Log(new Error(error));
+        }
+    }
+}
