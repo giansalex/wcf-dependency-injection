361288d0d0b50a7a7c9c5dc01e71cdd2ec03750e
Giancarlos Salas <giansalex@gmail.com>
2018-03-08 17:40:27 -0500

    complete global test

---
+++ WcfService.Tests/GlobalTests.cs
@@ -0,0 +1,24 @@
+﻿using Autofac.Integration.Wcf;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace WcfService.Tests
+{
+    [TestFixture]
+    public class GlobalTests
+    {
+        [Test]
+        public void Start()
+        {
+            var method = typeof(Global).GetMethod("Application_Start",
+                        BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+            Assert.NotNull(method);
+            Assert.Null(AutofacHostFactory.Container);
+
+            method.Invoke(null, new object[] { null, null});
+
+            Assert.NotNull(AutofacHostFactory.Container);
+        }
+    }
+}
