7fe78f40cea14ab15d22e153533cc7fb2ee90bf3
Giancarlos Salas <giansalex@gmail.com>
2018-03-08 16:22:13 -0500

    update autofac and set in testing

---
+++ WcfService.Tests/Config/BootstrapTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService.Config;
+
+namespace WcfService.Tests.Config
+{
+    [TestFixture]
+    public class BootstrapTests
+    {
+        [Test]
+        public void SuccessResolve()
+        {
+            var container = Bootstrap.Execute();
+        }
+    }
+}
