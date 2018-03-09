494de576d25f29eb7cbf8c5b17464dfe6ada41ef
Giancarlos Salas <giansalex@gmail.com>
2018-03-09 16:35:40 -0500

    add calculator spec

---
+++ WcfService.Specs/Services/CalculatorServiceSpec.cs
@@ -0,0 +1,31 @@
+﻿using Machine.Specifications;
+using WcfService.Services;
+using Moq;
+using FluentAssertions;
+
+namespace WcfService.Specs.Services
+{
+    [Subject("Operation")]
+    public abstract class CalculatorServiceSpec
+    {
+        Establish context = () => {
+            var mock = new Mock<IOperation>();
+            mock
+                .Setup(r => r.Execute(Moq.It.IsAny<int>(), Moq.It.IsAny<int>()))
+                .Returns((int a, int b) => a + b);
+
+            Subject = new CalculatorService(mock.Object);
+        };
+
+        Because of = async () => {
+            Token = await Subject.Operation(2, 4);
+        };
+
+        Machine.Specifications.It should_be_the_sum = () => {
+            Token.Should().Be(20);
+        };
+
+        static CalculatorService Subject;
+        static int Token;
+    }
+}
